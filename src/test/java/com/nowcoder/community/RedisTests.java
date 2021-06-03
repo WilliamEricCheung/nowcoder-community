40efdaa853f636f4722b065327348a1ba22d16fb
William Eric Cheung <williamericcheung@outlook.com>
2021-06-03 17:08:42 +0800

    整合了Redis并学习了API

---
+++ src/test/java/com/nowcoder/community/RedisTests.java
@@ -0,0 +1,62 @@
+package com.nowcoder.community;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.core.BoundValueOperations;
+import org.springframework.data.redis.core.RedisOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SessionCallback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = NowcoderCommunityApplication.class)
+public class RedisTests {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Test
+    public void testStrings(){
+        String redisKey = "test:count";
+        redisTemplate.opsForValue().set(redisKey, 1);
+        System.out.println(redisTemplate.opsForValue().get(redisKey));
+        System.out.println(redisTemplate.opsForValue().increment(redisKey));
+        System.out.println(redisTemplate.opsForValue().decrement(redisKey));
+    }
+
+    // 多次访问同一个key
+    @Test
+    public void testBoundOperation(){
+        String redisKey = "test:count";
+        BoundValueOperations operations = redisTemplate.boundValueOps(redisKey);
+        operations.increment();
+        operations.increment();
+        operations.increment();
+        operations.increment();
+        operations.increment();
+        System.out.println(operations.get());
+    }
+
+    // 编程式事务
+    @Test
+    public void testTransactions(){
+        Object obj = redisTemplate.execute(new SessionCallback() {
+            @Override
+            public Object execute(RedisOperations redisOperations) throws DataAccessException {
+                String redisKey = "test:tx";
+                redisOperations.multi(); // 启动事务
+                redisOperations.opsForSet().add(redisKey, "zhangsan");
+                redisOperations.opsForSet().add(redisKey, "lisi");
+                redisOperations.opsForSet().add(redisKey, "wangwu");
+                System.out.println(redisOperations.opsForSet().members(redisKey));
+                return redisOperations.exec();
+            }
+        });
+        System.out.println(obj);
+    }
+}
