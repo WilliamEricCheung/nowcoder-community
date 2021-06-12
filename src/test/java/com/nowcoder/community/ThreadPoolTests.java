631f9b955acd53a6bd8c1d2504a629b917ecbfe3
williamericcheung <williamericcheung@outlook.com>
2021-06-12 11:25:09 +0800

    学习了部分任务执行和调度

---
+++ src/test/java/com/nowcoder/community/ThreadPoolTests.java
@@ -0,0 +1,114 @@
+package com.nowcoder.community;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Date;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = NowcoderCommunityApplication.class)
+public class ThreadPoolTests {
+
+    // JDK普通线程池
+    private ExecutorService executorService = Executors.newFixedThreadPool(5);
+
+    // JDK可执行定时任务的线程池
+    private ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(5);
+
+    // Spring普通线程池
+    @Autowired
+    private ThreadPoolTaskExecutor taskExecutor;
+
+    // Spring可执行定时任务的线程池
+    @Autowired
+    private ThreadPoolTaskScheduler taskScheduler;
+
+    private void sleep(long m){
+        try {
+            Thread.sleep(m);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // 1. JDK普通线程池
+    @Test
+    public void testExecutorService(){
+         Runnable task = new Runnable() {
+             @Override
+             public void run() {
+                 log.debug("Hello ExecutorService");
+                 System.out.println("Hello ExecutorService");
+             }
+         };
+
+        for (int i = 0; i < 10; i++) {
+            executorService.submit(task);
+        }
+
+        sleep(10000);
+    }
+
+    // 2. JDK执行定时任务的线程池
+    @Test
+    public void testScheduledExecutorService(){
+        Runnable task = new Runnable() {
+            @Override
+            public void run() {
+                log.debug("Hello ScheduledExecutorService");
+                System.out.println("Hello ScheduledExecutorService");
+            }
+        };
+
+        scheduledExecutorService.scheduleAtFixedRate(task, 10000, 1000, TimeUnit.MILLISECONDS);
+
+        sleep(30000);
+    }
+
+    // 3. Spring普通线程池
+    @Test
+    public void testThreadPoolTaskExecutor(){
+        Runnable task = new Runnable() {
+            @Override
+            public void run() {
+                log.debug("Hello ThreadPoolTaskExecutor");
+                System.out.println("Hello ThreadPoolTaskExecutor");
+            }
+        };
+        for (int i = 0; i < 10; i++) {
+            taskExecutor.submit(task);
+        }
+        sleep(10000);
+    }
+
+    // 3. Spring可执行定时任务的线程池
+    @Test
+    public void testThreadPoolTaskScheduler(){
+        Runnable task = new Runnable() {
+            @Override
+            public void run() {
+                log.debug("Hello ThreadPoolTaskScheduler");
+                System.out.println("Hello ThreadPoolTaskScheduler");
+            }
+        };
+
+        Date startTime = new Date(System.currentTimeMillis() + 10000);
+        taskScheduler.scheduleAtFixedRate(task, startTime, 1000);
+
+        sleep(30000);
+    }
+
+}
